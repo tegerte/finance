861dc24a94704daae97bf7f34796e1c49853f70d
tegerte <home@tassilo-egerter.de>
2025-11-18 22:56:13 +0100

    some formatting

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 from datetime import datetime
 from scipy.optimize import newton
 
+# Eingabedaten: Datum und Betrag (Einzahlungen negativ, Auszahlungen positiv)
 CASHFLOWS = [
-        (datetime(2025, 3, 23), -16000),  # Start Einzahlung
-        (datetime(2025, 6, 1), 12000),  # Abhebung
-        (datetime(2025, 8, 11), -700),  # Einzahlung
-        (datetime(2025, 11, 18), 4921),  # aktueller Wert inkl. Zinsen
-    ]
-
-
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    (datetime(2025, 3, 23), -16000),  # Start Einzahlung
+    (datetime(2025, 6, 1), 12000),  # Abhebung
+    (datetime(2025, 8, 11), -700),  # Einzahlung
+    (datetime(2025, 11, 18), 4921),  # aktueller Wert inkl. Zinsen
+]
 
 
 def npv(rate):
@@ -43,30 +33,17 @@ def xirr(guess=0.05):
         return None
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-
     start_date = CASHFLOWS[0][0]
-
     # Umwandlung der Daten in Jahre seit Start und Beträge
     times = [(cf[0] - start_date).days / 365.25 for cf in CASHFLOWS]
     # Für jeden Cashflow wird die Zeitdifferenz zum Startdatum in Jahren berechnet.
     # 365.25 wegen Schaltjahr
-
     amounts = [cf[1] for cf in CASHFLOWS]
-
     # Berechnung des Net Present Value
-
     result = xirr()
     if result is not None:
         print(f"Die berechnete Rendite (IRR) beträgt: {result * 100:.2f}% pro Jahr")
     else:
         print("Die Rendite konnte nicht berechnet werden.")
-
-    # print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-# Eingabedaten: Datum und Betrag (Einzahlungen negativ, Auszahlungen positiv)
